8bb1b86a42c8bb08b3f90f047a78194fe7198d6d
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-10-28 05:55:10 -0500

    started setting up scraper

--- index.js
+++ index.js
@@ -5,15 +5,31 @@ const cheerio = require("cheerio");
 
 const app = express();
 
+const articles = [];
+
 app.get("/", (req, res) => {
   res.json("Welcome to my crypto tracking API");
 });
 
 app.get("/news", (req, res) => {
-  axios.get("https://cryptonews.com/").then((response) => {
-    const html = response.data;
-    console.log(html);
-  });
+  axios
+    .get("https://cryptonews.com/")
+    .then((response) => {
+      const html = response.data;
+      // console.log(html);
+      const data = cheerio.load(html);
+
+      data("a.article__title", html).each(function () {
+        const title = data(this).text();
+        const url = data(this).attr("href");
+        articles.push({
+          title,
+          url,
+        });
+      });
+      res.json(articles);
+    })
+    .catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
