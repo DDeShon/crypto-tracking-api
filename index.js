8d6eb10dfb4290b2bc12e708a00e133ac7685d4e
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-10-28 06:22:05 -0500

    setting up scraper

--- index.js
+++ index.js
@@ -5,26 +5,49 @@ const cheerio = require("cheerio");
 
 const app = express();
 
+// const sources = [
+//     {
+//         name: 'cryptonews',
+//         address: 'https://cryptonews.com/'
+//     },
+//     {
+//         name: 'cryptocurrencynews',
+//         address: 'https://cryptocurrencynews.com/'
+//     },
+//     {
+//         name: 'coindesk',
+//         address: 'https://www.coindesk.com/'
+//     }
+// ]
+
 const articles = [];
 
+// sources.forEach(source => {
+//     axios.get(source.address)
+//     .then(response => {
+//         const html = response.data;
+//       // console.log(html);
+//       const data = cheerio.load(html);
+//     })
+// })
+
 app.get("/", (req, res) => {
-  res.json("Welcome to my crypto tracking API");
+  res.json("Welcome to my crypto news tracking API");
 });
 
 app.get("/news", (req, res) => {
   axios
-    .get("https://cryptonews.com/")
+    .get("https://coinmarketcap.com/")
     .then((response) => {
       const html = response.data;
-      // console.log(html);
       const data = cheerio.load(html);
 
-      data("a.article__title", html).each(function () {
+      data("a.Link__A-eh4rrz-0", html).each(function () {
         const title = data(this).text();
         const url = data(this).attr("href");
         articles.push({
           title,
-          url,
+          href,
         });
       });
       res.json(articles);
